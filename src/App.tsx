193459f6f91157e595ec3cec95022d243743ecca
PRATIK KUMAR BISWAL <pratik16bis@gmail.com>
2025-08-04 21:36:24 +0530

    Initialized component library from shadcn for the project.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from "react";
+import { Button } from "@/components/ui/button"
 
 const App: React.FC = () => {
   return (
     <div className="bg-blue-500 text-white p-4">
       <div>Coming soon...</div>
       <h3>Vite + React</h3>
+      <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
     </div>
   );
 };
