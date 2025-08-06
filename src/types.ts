dea222ef9f015eff257d9e5467874ca183d7cba0
PRATIK KUMAR BISWAL <pratik16bis@gmail.com>
2025-08-06 08:05:22 +0530

    featureUpdate: Defined core data structures for the canvas.

---
+++ src/types.ts
@@ -0,0 +1,43 @@
+//Defining the shape of data that our app will adhere to.
+
+//represents an RGB color
+export type Color = {
+    r: number;
+    g: number;
+    b: number;
+};
+
+//represents an camera component, which is responsible for rendering the specific viewpoint, which represents the user's view of the canvas.
+export type Camera = {
+    x: number;
+    y: number;
+};
+
+//different layer types we have that we can add to the canvas
+export type LayerType = 'Rectangle' | 'Circle' | 'Text' | 'Triangle';
+
+export type RectangleLayer = {
+    type: 'Rectangle';
+    x: number;
+    y: number;
+    height: number;
+    width: number;
+    fill: Color;
+    value?: string;
+};
+
+//a union of all possible layer types in the canvas
+export type Layer = RectangleLayer;
+
+//represents a specific point on the canvas
+export type coordinate = {
+    x: number;
+    y: number;
+};
+
+//represents the state of the entire canvas
+export type CanvasState = {
+    currentlySelectedLayerId: string | null;
+    layers: Record<string, Layer>;
+    camera: Camera;
+};
